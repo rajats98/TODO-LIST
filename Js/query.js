c4dd4be864e971cb6a6cea7a7fcfb3d3b935cc9f
rajat.sharma <rajats1.kv2srg@gmail.com>
2019-02-05 15:18:05 +0530

    Functional ver 0.2

--- Js/query.js
+++ Js/query.js
@@ -5,6 +5,16 @@ $("#plus").on("click",function() {
 $("#newTodo").keypress(function(key){
 	if(key.which===13 && $(this).val()!="")
 	{
-		
+		 $('table').append("<tr class=\"entry\"> <td class=\"trash\"> <i class=\"far fa-trash-alt\" ></i> </td> <td class=\"todo\">"+this.value+"</td> </tr>");
+		$(this).val("");
 	}
+});
+
+$("table").on("click",".trash",function(){
+	$(this).parent().remove();
+});
+
+$("table").on("click",".todo",function(){
+	$(this).toggleClass("done");
+
 });
